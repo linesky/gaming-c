e7d17c0e3801bacd3313756ae2cadbb311d991d5
sky line <32512284+linesky@users.noreply.github.com>
2022-08-23 14:32:47 +0100

    ram2d map read and write

--- ram2d.c
+++ ram2d.c
@@ -1,5 +1,6 @@
 //https://wasdk.github.io/WasmFiddle/
 #include<string.h>
+#include<stdio.h>
   #define xx 8
   #define yy 8
   int i[xx*yy+2];
@@ -26,3 +27,27 @@ int ram2d(int x,int y,int writes,int value){
 }
 
 
+int main(){
+	int x=0;
+	int y=0;
+	int dx=0;
+	int dy=0;
+	int errs=0;
+	x=ram2d(0,0,0,0);
+	y=ram2d(0+1,0,0,0);
+		for(dy=0;dy<y;dy++){
+			for(dx=0;dx<x;dx++){
+				ram2d(dx+2,dy,1,dx+(dy*xx));
+			}
+		}
+
+		for(dy=0;dy<y;dy++){
+			for(dx=0;dx<x;dx++){
+				errs=ram2d(dx+2,dy,0,0);
+				if(errs!=dx+(dy*xx))puts("---error");
+				printf("x %d,y %d = %d\n",dx,dy,errs);
+			}
+		}
+
+
+}
