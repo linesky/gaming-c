1c8a5a2cbf3c105b9752e329e1bf6ed9ca3d740e
sky line <32512284+linesky@users.noreply.github.com>
2022-08-23 15:51:58 +0100

    encoders to hide a password from webasm

--- encoderX.c
+++ encoderX.c
@@ -1,9 +1,20 @@
 //https://wasdk.github.io/WasmFiddle/
+#include<stdio.h>
 int encodeX(int a){ 
+  int size=16;
   int ii=-1;
   int i[]={5,7,9,3,71,10,10,12,4,12,2,1,2,3,15,16};
-  if(a>-1 && a<16){
+  if(a>-1 && a<size){
 	  ii=i[a];
   }else{ii=-1;}
   return ii;
+}
+
+int main(){
+	int d=0;
+	int e=0;
+	printf("\033[42;37m\n");
+	e=16;
+	for(d=0;d<e;d++)printf("%d=%d\n",d,encodeX(d));
+	return 0;
 }
