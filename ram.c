537ba63e9fb25697a344125823059b63c0b79982
sky line <32512284+linesky@users.noreply.github.com>
2022-08-23 14:48:32 +0100

    ram memory

--- ram.c
+++ ram.c
@@ -1,7 +1,8 @@
 //https://wasdk.github.io/WasmFiddle/
 #include<string.h>
+#include<stdio.h>
 #define l 512
-char i[l+2];
+int i[l+2];
 int ram(int a,int writes,int value){ 
   int ii=0;
   if(a>0 && a<l+1){
@@ -16,4 +17,15 @@ int ram(int a,int writes,int value){
   return ii;
 }
 
-
+int main(){
+	int d=0;
+	int v=0;
+	int sizes=ram(0,0,0);
+	for(d=0;d<sizes;d++)ram(d+1,1,d);
+	for(d=0;d<sizes;d++){
+		v=ram(d+1,1,d);
+		if(v!=d)puts("error--------------");
+		printf("%d=%d\n",d,v);
+	}
+	
+}
