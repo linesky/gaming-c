bcbcc2fcffbedf75a59ec152712f49eb3661b124
sky line <32512284+linesky@users.noreply.github.com>
2022-08-23 13:55:39 +0100

    Delete add.js

--- add.js
+++
@@ -1,4 +0,0 @@
-//https://wasdk.github.io/WasmFiddle/
-var wasmModule = new WebAssembly.Module(wasmCode);
-var wasmInstance = new WebAssembly.Instance(wasmModule, wasmImports);
-log(wasmInstance.exports.add(1,2));
